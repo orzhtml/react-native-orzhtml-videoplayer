c032dc98a27d43333972ef576e50fae4ac192fd9
Carson <375162094@qq.com>
2021-01-05 15:53:19 +0800

    update

--- example/video-player/index.js
+++ example/video-player/index.js
@@ -1,16 +1,32 @@
-import { formatTime, statusBarHeight, screenWidth, screenHeight, defaultVideoHeight, defaultVideoWidth } from './libs/Utils'
-import { isIPhoneX, isIPhoneXMax, isIPhone12, isIPhone12Max, isIPhoneWithMonobrow, isExpo, getStatusBarHeight } from './libs/StatusBarHeight'
+import {
+  formatTime, statusBarHeight, screenWidth,
+  screenHeight, defaultVideoHeight, defaultVideoWidth
+} from './libs/Utils'
+
+import {
+  isIPhoneX, isIPhoneXMax, isIPhone12, isIPhone12Max,
+  isIPhoneWithMonobrow, getStatusBarHeight
+} from './libs/StatusBarHeight'
+
 import VideoPlayer from './src/VideoPlayer'
+
 import VideoModal from './src/VideoModal'
 
 export {
   VideoPlayer,
   VideoModal,
+
   formatTime,
   statusBarHeight,
   screenWidth,
   screenHeight,
   defaultVideoHeight,
   defaultVideoWidth,
-  isIPhoneX, isIPhoneXMax, isIPhone12, isIPhone12Max, isIPhoneWithMonobrow, isExpo, getStatusBarHeight
+
+  isIPhoneX,
+  isIPhoneXMax,
+  isIPhone12,
+  isIPhone12Max,
+  isIPhoneWithMonobrow,
+  getStatusBarHeight
 }
